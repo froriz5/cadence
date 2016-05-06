b9fcc01e4d49b26503904bed29289e5643e42760
Felipe Roriz <froriz50@gatech.edu>
2016-05-05 19:44:05 -0500

    blah

--- mobile/src/main/kotlin/com/feliperoriz/cadence/MainActivity.kt
+++ mobile/src/main/kotlin/com/feliperoriz/cadence/MainActivity.kt
@@ -2,7 +2,9 @@ package com.feliperoriz.cadence
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by Felipe Roriz on 5/5/16.
@@ -12,7 +14,7 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        
         Toast.makeText(this, "Testing", Toast.LENGTH_LONG).show()
     }
 }
