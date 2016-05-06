98d6908c717a8a6181f2aebc3c3534306f1f97ee
Felipe Roriz <froriz50@gatech.edu>
2016-05-05 19:13:57 -0500

    Initial Commit.

---
+++ mobile/src/main/kotlin/com/feliperoriz/cadence/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.feliperoriz.cadence
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+/**
+ * Created by Felipe Roriz on 5/5/16.
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Toast.makeText(this, "Testing", Toast.LENGTH_LONG).show()
+    }
+}
