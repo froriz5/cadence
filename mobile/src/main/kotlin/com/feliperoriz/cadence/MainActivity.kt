c4918d6763e1bb34c0d755903ee8967b243cb612
Felipe Roriz <froriz50@gatech.edu>
2016-05-07 10:14:32 -0500

    Added Bottom Drawer library and inital setup.

--- mobile/src/main/kotlin/com/feliperoriz/cadence/MainActivity.kt
+++ mobile/src/main/kotlin/com/feliperoriz/cadence/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.feliperoriz.cadence
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by Felipe Roriz on 5/5/16.
  */
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        Toast.makeText(this, "Testing", Toast.LENGTH_LONG).show()
+
+        val library = AHBottomNavigationItem("Library", getDrawable(R.drawable.icon_library), getColor(R.color.colorPrimary));
+        val search = AHBottomNavigationItem("Search", getDrawable(R.drawable.icon_search), getColor(R.color.colorPrimary));
+        val settings = AHBottomNavigationItem("Settings", getDrawable(R.drawable.icon_settings), getColor(R.color.colorPrimary));
+        val item4 = AHBottomNavigationItem("Item 4", getDrawable(R.drawable.icon_settings), getColor(R.color.colorPrimary));
+        val item5 = AHBottomNavigationItem("Item 5", getDrawable(R.drawable.icon_settings), getColor(R.color.colorPrimary));
+        nav_drawer.addItem(library)
+        nav_drawer.addItem(search)
+        nav_drawer.addItem(settings)
+        nav_drawer.addItem(item4)
+        nav_drawer.addItem(item5)
+        nav_drawer.currentItem = 0
     }
 }
